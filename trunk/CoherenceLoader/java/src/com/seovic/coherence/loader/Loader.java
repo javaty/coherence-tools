ad014727758325cf375d7a177372b9f56ac0e827
ivancikic <ivancikic@3f307b9a-4c6c-11de-9ee4-913ee803a954>
2009-06-10 21:32:57 +0000

    Added XmlSource and XmlTarget. Refactoring.

--- trunk/CoherenceLoader/java/src/com/seovic/coherence/loader/Loader.java
+++ trunk/CoherenceLoader/java/src/com/seovic/coherence/loader/Loader.java
@@ -1,18 +1,17 @@
 package com.seovic.coherence.loader;
 
-import com.seovic.coherence.test.objects.Country;
-import com.seovic.coherence.loader.mapper.ExpressionPropertyMapper;
-import com.seovic.coherence.loader.source.CsvFileSource;
-import com.seovic.coherence.loader.source.CoherenceCacheSource;
-import com.seovic.coherence.loader.target.CoherenceCacheTarget;
-import com.seovic.coherence.loader.target.CsvFileTarget;
-import com.tangosol.net.CacheFactory;
-import com.tangosol.net.NamedCache;
+import com.seovic.coherence.loader.source.XmlSource;
+import com.seovic.coherence.loader.target.AbstractBaseTarget;
+import org.w3c.dom.Document;
 
-import java.io.Reader;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.InputStreamReader;
-import java.io.Writer;
-import java.io.FileWriter;
+import java.io.Reader;
+import java.io.StringWriter;
 
 /**
  * @author ic  2009.06.09
@@ -36,29 +35,61 @@ public class Loader {
 
     public static void main(String[] args) throws Exception {
 
-        Reader countriesReader = new InputStreamReader(Loader.class.getClassLoader().getResourceAsStream("countries.csv"));
+        Reader countriesReader = new InputStreamReader(Loader.class.getClassLoader().getResourceAsStream("countries-ns.xml"));
 
-        Source source = new CsvFileSource(countriesReader);
+        Source source = new XmlSource(countriesReader);
 
-        CoherenceCacheTarget target = new CoherenceCacheTarget("countries", Country.class);
-        target.registerPropertyMapper("name", new ExpressionPropertyMapper("code + ':' + ime.toUpperCase()", null));
 
-        Loader loader = new Loader(source, target);
+        //CoherenceCacheTarget target = new CoherenceCacheTarget("countries", Country.class);
+        //target.registerPropertyMapper("name", new ExpressionPropertyMapper("code + ':' + ime.toUpperCase()"));
+//        Writer writer = new FileWriter("countries-out.xml", false);
+//        Map<String, String> nmsp = new HashMap<String, String>();
+//        nmsp.put("v", "validationUri");
+//        nmsp.put(null, "defaultNs");
+//        nmsp.put("c", "jstlUri");
+//        nmsp.put("config", "sigfeConfigUri");
+//        nmsp.put("id", "idUri");
+//
+//        XmlTarget target = new XmlTarget(writer, nmsp,  "countries", "country", "id:@code,@name,config:formalName,config:capital,currencySymbol,currencyName,v:telephonePrefix,v:domain");
+//        target.registerPropertyMapper("name", new ExpressionPropertyMapper("ime"));
+
+
+        Loader loader = new Loader(source, new XmlReporterTarget());
         loader.load();
-        
-        NamedCache cache = CacheFactory.getCache("countries");
-        System.out.println("Imported " + cache.size() + " items.");
-        System.out.println(cache.get("SRB"));
-        System.out.println(cache.get("ESH"));
 
-        Source source2 = new CoherenceCacheSource("countries");
+//        NamedCache cache = CacheFactory.getCache("countries");
+//        System.out.println("Imported " + cache.size() + " items.");
+//        System.out.println(cache.get("SRB"));
+//        System.out.println(cache.get("ESH"));
+//
+//        Source source2 = new CoherenceCacheSource("countries");
 
-        Writer writer = new FileWriter("countries-out.csv", false);
-        CsvFileTarget target2 = new CsvFileTarget(writer, "code", "name", "domain");
-        target2.registerPropertyMapper("name", new ExpressionPropertyMapper("code + ':' + name.toLowerCase()", null));
+//        Writer writer = new FileWriter("countries-out.xml", false);
+//        XmlTarget target2 = new XmlTarget(writer, "countries", "country", "code,name,formalName,capital,currencySymbol,currencyName,telephonePrefix,domain");
+//        target2.registerPropertyMapper("name", new ExpressionPropertyMapper("code + ':' + name.toLowerCase()", null));
+//
+//        Loader loader2 = new Loader(source2, target2);
+//        loader2.load();
 
-        Loader loader2 = new Loader(source2, target2);
-        loader2.load();
-        
     }
+
+    public static class XmlReporterTarget extends AbstractBaseTarget {
+
+        public void importSingle(Object item) {
+            try {
+                DOMSource domSource = new DOMSource((Document) item);
+                StringWriter writer = new StringWriter();
+                StreamResult result = new StreamResult(writer);
+                TransformerFactory tf = TransformerFactory.newInstance();
+                Transformer transformer = tf.newTransformer();
+                transformer.transform(domSource, result);
+                System.out.println(writer.toString());
+            }
+            catch (TransformerException ignore) {
+            }
+        }
+    }
+
+
 }
+
