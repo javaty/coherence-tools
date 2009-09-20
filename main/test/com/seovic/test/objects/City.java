cf00d0089715cfd82bd0d17e588626856011164d
aleks <aleks@seovic.com>
2009-09-20 06:23:30 +0000

    Moved test objects to test source

--- main/test/com/seovic/test/objects/City.java
+++ main/test/com/seovic/test/objects/City.java
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package com.seovic.coherence.test.objects;
+package com.seovic.test.objects;
 
 
 import com.tangosol.io.pof.PortableObject;
