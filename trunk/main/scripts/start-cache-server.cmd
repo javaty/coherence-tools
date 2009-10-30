78b13e0cbc1619f5d43405d663745c9b7e38f828
ivancikic@gmail.com <ivancikic@gmail.com@3f307b9a-4c6c-11de-9ee4-913ee803a954>
2009-10-30 19:28:15 +0000

    Updated batch file for staring coherence.

--- trunk/main/scripts/start-cache-server.cmd
+++ trunk/main/scripts/start-cache-server.cmd
@@ -15,10 +15,7 @@ set memory=128m
 
 :start
 
-if not exist "%DEV_ROOT%\lib\coherence\coherence.jar" goto instructions
-
 if "%JAVA_HOME%"=="" (set JAVA_EXEC=java) else (set JAVA_EXEC=%JAVA_HOME%\bin\java)
-
 
 :launch
 
@@ -29,7 +26,7 @@ set SYS_OPTS="-Dtangosol.coherence.cacheconfig=coherence-cache-config.xml"
 set CLASSPATH=%DEV_ROOT%\cfg
 set CLASSPATH=%CLASSPATH%;%DEV_ROOT%\build\classes
 set CLASSPATH=%CLASSPATH%;%DEV_ROOT%\build\tests
-set CLASSPATH=%CLASSPATH%;%DEV_ROOT%\lib\coherence\coherence.jar
+set CLASSPATH=%CLASSPATH%;%COHERENCE_HOME%\lib\coherence.jar
 set CLASSPATH=%CLASSPATH%;%DEV_ROOT%\lib\groovy-1.6.3\groovy-all-1.6.3.jar
 set CLASSPATH=%CLASSPATH%;%DEV_ROOT%\lib\spring-framework-3.0.0-RC1\org.springframework.beans-3.0.0.RC1.jar
 set CLASSPATH=%CLASSPATH%;%DEV_ROOT%\lib\spring-framework-3.0.0-RC1\org.springframework.context-3.0.0.RC1.jar
@@ -42,12 +39,6 @@ set CLASSPATH=%CLASSPATH%;%DEV_ROOT%\lib\log4j-1.2.15\log4j-1.2.15.jar
 
 "%JAVA_EXEC%" -server -showversion "%JAVA_OPTS%" "%SYS_OPTS%" -cp "%CLASSPATH%" com.tangosol.net.DefaultCacheServer %1
 
-goto exit
-
-:instructions
-
-echo Usage:
-echo   ^<DEV_ROOT^>\bin\sigfe-cache-server.cmd
 goto exit
 
 :exit
