109bfabddcc238fd7dd102809df3ecf8596fb89c
ivancikic <ivancikic@3f307b9a-4c6c-11de-9ee4-913ee803a954>
2011-06-17 13:57:41 +0000

    Simple pause scheduled job implementation.

--- main/src/com/seovic/coherence/scheduling/ScheduledJob.java
+++ main/src/com/seovic/coherence/scheduling/ScheduledJob.java
@@ -2,8 +2,10 @@ package com.seovic.coherence.scheduling;
 
 
 import java.io.Serializable;
+
 import java.util.List;
 import java.util.Arrays;
+
 import org.quartz.JobDetail;
 import org.quartz.Trigger;
 
@@ -12,15 +14,16 @@ import org.quartz.Trigger;
  * @author Aleksandar Seovic  2011.04.19
 */
 public class ScheduledJob implements Serializable {
-    private JobDetail job;
+    private JobDetail     job;
     private List<Trigger> triggers;
+    private boolean       paused;
 
     public ScheduledJob(JobDetail job, Trigger... schedule) {
         this(job, Arrays.asList(schedule));
     }
 
     public ScheduledJob(JobDetail job, List<Trigger> triggers) {
-        this.job = job;
+        this.job      = job;
         this.triggers = triggers;
     }
 
@@ -30,5 +33,13 @@ public class ScheduledJob implements Serializable {
 
     public List<Trigger> getTriggers() {
         return triggers;
+    }
+
+    public boolean isPaused() {
+        return paused;
+    }
+
+    public void setPaused(boolean paused) {
+        this.paused = paused;
     }
 }
